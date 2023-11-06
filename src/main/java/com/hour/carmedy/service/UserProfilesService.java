f7a316eb1bcf19cc93e8bd7c460c4c5c3786e77f
Mustafa <mustafa.alvi.98@gmail.com>
2023-11-05 21:57:08 -0500

    Remove unused imports

--- src/main/java/com/hour/carmedy/service/UserProfilesService.java
+++ src/main/java/com/hour/carmedy/service/UserProfilesService.java
@@ -3,9 +3,7 @@ package com.hour.carmedy.service;
 import com.hour.carmedy.model.UserProfile;
 import com.hour.carmedy.repository.UserProfilesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.data.domain.Page;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 //Purpose: To serve other daters' profiles to the client/user
