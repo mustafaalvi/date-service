188410992b1c248332377ea4b8a136ff05becacf
Mustafa <mustafa.alvi.98@gmail.com>
2023-11-05 21:43:33 -0500

    Enable the db configuration pick up from app.yml

--- src/main/java/com/hour/carmedy/Main.java
+++ src/main/java/com/hour/carmedy/Main.java
@@ -5,14 +5,17 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String connectionString = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/test";
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            MongoDatabase database = mongoClient.getDatabase("your-database-name");
-            MongoCollection<Document> collection = database.getCollection("your-collection-name");
-        }
+//        String connectionString = "mongodb+srv://mualvi:iloverona@cluster0.fnsv9.gcp.mongodb.net/?retryWrites=true&w=majority";
+//        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+//            MongoDatabase database = mongoClient.getDatabase("DateApp");
+//            MongoCollection<Document> collection = database.getCollection("UserProfiles");
+//        }
+
     }
 }
